0eb1b264d4a5b60f305793d87651a730cda99454
Andrii Kicha <andriik@onehourtranslation.com>
2018-08-04 14:30:32 +0300

    Change rest api endpoint + remove unnecessaery field in config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,12 @@
 export default {
   endpoints: {
-    root: 'http://pudgepredicts.gq/wp-json/wp/v2',
+    root: 'http://sportexp.gq/wp-json/wp/v2',
     admin: 'http://localhost:3000',
     getPosts(extra = '') {
       return `${this.root}/posts?_embed${extra}`
     },
     getCategories() {
       return `${this.root}/categories`
-    },
-    getHats() {
-      return `${this.admin}/hats`
     }
   }
 };
